aa617ffa1cc37a06ce89a02fd8b38963e09fd4f3
liuyinz <liuyinz95@gmail.com>
2023-09-04 02:36:06 +0800

    style: adjust comments width

--- duplexer.el
+++ duplexer.el
@@ -26,9 +26,9 @@
 
 ;;; Commentary:
 
-;; This package provides method to handle conflicts between local minor modes and reuse rules.
-;; When a CALLER mode enable, call related CALLEE modes with arguments and restore them
-;; automatically after the CALLER mode disabled.
+;; This package provides method to handle conflicts between local minor modes
+;; and reuse rules.  When a CALLER mode enable, call related CALLEE modes with
+;; arguments and restore them automatically after the CALLER mode disabled.
 
 ;;; Code:
 
